faf38c76c08eb87cf6fe3c8f23e6f9da9d63cb5e
jannj <jannjacobson1997@gmail.com>
2023-02-19 15:59:20 +0100

    fix deck card style

--- frontend/src/components/card/DeckCard.vue
+++ frontend/src/components/card/DeckCard.vue
@@ -11,6 +11,7 @@ defineProps<{
     :class="{
       'text-white dark:text-gray-900 -translate-y-1.5': selected,
       'border-gray-300 dark:border-gray-600 cursor-default': disabled,
+      'border-blue-600 dark:border-blue-400': !disabled,
       'bg-blue-600 dark:bg-blue-400': selected && !disabled,
       'bg-gray-300 dark:bg-gray-600 dark:text-gray-900': selected && disabled,
       'text-gray-300 dark:text-gray-600': !selected && disabled,
@@ -18,7 +19,7 @@ defineProps<{
     }"
     class="
       border-2 rounded-md w-11 h-16 text-xl font-semibold flex items-center justify-center
-      mx-auto transition duration-150 ease-out border-blue-600 dark:text-blue-400 dark:border-blue-400
+      mx-auto transition duration-150 ease-out
     "
     type="button"
   >
