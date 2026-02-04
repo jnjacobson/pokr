e5a823f88ff9af2935653a5ee5d4d92ef60e408f
Jan Jacobson <j.jacobson@demv.de>
2026-02-04 16:06:06 +0100

    fix: change order of deck

--- backend/lib/pokr/game.ex
+++ backend/lib/pokr/game.ex
@@ -1,6 +1,6 @@
 defmodule Pokr.Game do
   defstruct [
-    deck: ~w(0 ½ 1 2 3 5 8 13 20 40 100 ☕ ?),
+    deck: ~w(0 ½ 1 2 3 5 8 13 20 40 100 ? ☕),
     are_cards_revealed: false,
     player_count: 0,
   ]
